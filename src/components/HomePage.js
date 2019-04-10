ccd9fd1a161f57c6098ba1839ca5d74a35d0b8da
Kokila Widyaratna <kokilapc@gmail.com>
2019-04-11 01:06:55 +0530

    eslint fix

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.span`
+    display: block;
+    font-family: Poppins-Bold;
+    font-size: 30px;
+    color: #333333;
+    line-height: 1.2;
+    text-align: center;
+`;
 
 const HomePage = () => (
     <div className="container-login100">
         <div className="wrap-login100">
             <form className="login100-form validate-form">
-                <span className="login100-form-title p-b-26">
+                {/* <span className="login100-form-title p-b-26">
 						Sinhala Grammar Tool
-                </span>
+                </span> */}
+                <Title className="p-b-26">Sinhala Grammar Tool</Title>
 
                 <div>
                     <div className="textarea-container">
-                        <textarea
-                            placeholder="Insert the sentences here...">ඇය මට ගිය සතියේ ලියුමක් එව්වේය. මා ඇය හට එය සඳහා පිළිතුරක් කෑවේය.</textarea>
+                        <textarea placeholder="Insert the sentences here...">
+                            ඇය මට ගිය සතියේ ලියුමක් එව්වේය. මා ඇය හට එය සඳහා
+                            පිළිතුරක් කෑවේය.
+                        </textarea>
                         <div className="backdrop">
                             <div className="highlights" />
                         </div>
@@ -21,7 +34,6 @@ const HomePage = () => (
             </form>
         </div>
     </div>
-)
-
+);
 
 export default HomePage;
