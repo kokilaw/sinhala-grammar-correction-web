99369ed9b763572df862f49b164aa10334c85a13
Kokila Widyaratna <kokilapc@gmail.com>
2019-04-29 22:07:37 +0530

    Updates

---
+++ precache-manifest.ea4f6b4f502672b657a3325e78045386.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e63b93dfac2600782654e2b87910d681",
+    "url": "/sinhala-deep-grammar-demo/static/media/Poppins-SemiBold.e63b93df.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/sinhala-deep-grammar-demo/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "a4e11dda40531debd374e4c8b1dcc7f4",
+    "url": "/sinhala-deep-grammar-demo/static/media/Poppins-Medium.a4e11dda.ttf"
+  },
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/sinhala-deep-grammar-demo/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "3ebcd34b700373e262ff",
+    "url": "/sinhala-deep-grammar-demo/static/js/runtime~main.30935cc6.js"
+  },
+  {
+    "revision": "3fe4d35b6cfa83e528ba",
+    "url": "/sinhala-deep-grammar-demo/static/js/main.0b5de8a7.chunk.js"
+  },
+  {
+    "revision": "e3aef96c957d0eb9203b",
+    "url": "/sinhala-deep-grammar-demo/static/js/2.8e7b79bb.chunk.js"
+  },
+  {
+    "revision": "3fe4d35b6cfa83e528ba",
+    "url": "/sinhala-deep-grammar-demo/static/css/main.a68bac85.chunk.css"
+  },
+  {
+    "revision": "4e9f8e8ad984893c1a59d407e793aab3",
+    "url": "/sinhala-deep-grammar-demo/index.html"
+  }
+];
