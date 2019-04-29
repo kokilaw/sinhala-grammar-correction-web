cb07a14b1b90ad784dd6ba6e88103ae820396849
Kokila Widyaratna <kokilapc@gmail.com>
2019-04-29 22:06:28 +0530

    gcloud rest api connection and github page setup

--- src/components/HomePageTemp.js
+++ src/components/HomePageTemp.js
@@ -27,8 +27,14 @@ const ValidationError = () => (
                     - Should contain only sinhala unicode characters and ends
                     with a fullstop.
                 </li>
+                <li>
+                    - Numbers and other punctuation marks are not supported yet.
+                </li>
                 <li>- No additional white spaces are supported.</li>
-                <li>- Sentence should be within word limit 3-7.</li>
+                <li>
+                    - Only Sentence within word limit 3-7 are currently
+                    supported.
+                </li>
             </ul>
         </p>
         <hr />
